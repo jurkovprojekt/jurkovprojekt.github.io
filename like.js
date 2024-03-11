b75bed88e543112c00c196c0da7ed451eba450ad
EBugYT <89686363+EBugYT@users.noreply.github.com>
2024-03-11 18:58:40 +0000

    like v2

--- like.js
+++ like.js
@@ -1,15 +1,25 @@
-var pantryid = "42536e6a-f14f-498a-9180-37dcb088251d";
-var xhttp = new XMLHttpRequest();
-var url = 'https://getpantry.cloud/apiv1/pantry/'+ pantryid+'/basket/likes';
-var params = JSON.stringify({ "key": "value" });
+function like() {
+  var pantryid = "42536e6a-f14f-498a-9180-37dcb088251d";
+  var request = new XMLHttpRequest();
+  var apiUrl = 'https://getpantry.cloud/apiv1/pantry/42536e6a-f14f-498a-9180-37dcb088251d/basket/likes';
 
-xhttp.open("POST", url, true);
-xhttp.setRequestHeader("Content-type", "application/json");
+  request.open("GET", apiUrl, true);
+  var responseData
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      responseData = JSON.parse(request.responseText);
+      console.log(responseData);
+    }
+  };
 
-xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    console.log(xhttp.responseText);
-  }
-};
+  request.send();
 
-xhttp.send(params);
+  var xhttppost = new XMLHttpRequest()
+  var url = 'https://getpantry.cloud/apiv1/pantry/' + pantryid + '/basket/likes';
+  var params = JSON.stringify({ "likes": 12 });
+
+  xhttppost.open("POST", url, true);
+  xhttppost.setRequestHeader("Content-type", "application/json");
+
+  xhttppost.send(params);
+}
